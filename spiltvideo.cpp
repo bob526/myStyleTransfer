d445b2ffed797a735c83da7e6f2d47f91387c17a
nari <invoid@csie.io>
2017-07-28 19:52:19 +0800

    first commit

---
+++ spiltvideo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	if (argc<2) {
+		cout<<"Argument Error"<<endl;
+		return -1;
+	}
+	//cout<<argv[1]<<endl;
+	VideoCapture targetVideo();
+
+
+	return 0;
+}
