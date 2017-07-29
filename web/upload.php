ffe0e2ab12cf634ad9adaf5f5051d02299e03fd3
Chien <ccchuan103u@csie2.cs.ccu.edu.tw>
2017-07-29 20:41:27 +0800

    add ; in the end of line 6

--- web/upload.php
+++ web/upload.php
@@ -3,7 +3,7 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-$PHP_LIMIT_SIZE = 10000000
+$PHP_LIMIT_SIZE = 10000000;
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
