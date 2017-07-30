47f5f0339ab8ad68598aae57a3491d834e5af22c
nari <invoid@csie.io>
2017-07-30 21:17:14 +0800

    can't compile

--- spiltvideo.cpp
+++ spiltvideo.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <cstdlib>
+#include <string>
 
 using namespace cv;
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	if (argc<2) {
+	// Correct Command line argument: Exec video.mp4 time
+	if (argc<3) {
 		cout<<"Argument Error"<<endl;
 		return -1;
 	}
 	//cout<<argv[1]<<endl;
-	VideoCapture targetVideo();
+	int timeBetween = atoi(argv[2]);
+	string testStr(argv[1]); 
+	//cout<<timeBetween<<endl;
+	VideoCapture targetVideo;
+	targetVideo.open(testStr);
 
 
 	return 0;
