08b0c5045bf71de615a2df7def20633b281bc1cd
nari <invoid@csie.io>
2017-07-30 22:51:03 +0800

    comment some code

--- web/upload.php
+++ web/upload.php
@@ -28,10 +28,12 @@ if ($_FILES["fileToUpload"]["size"] > $PHP_LIMIT_SIZE) {
     $uploadOk = 0;
 }
 // Allow certain file formats
+/*
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
     echo "Sorry, only JPG, JPEG & PNG files are allowed.";
     $uploadOk = 0;
 }
+*/
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
