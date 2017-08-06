ffd12f54d7087ba4d5a8845905601a16f343cf57
nari <invoid@csie.io>
2017-08-07 01:41:44 +0800

    test with command line argument

--- web/auto_style.sh
+++ web/auto_style.sh
@@ -3,7 +3,9 @@
 
 #ssh -i ~/.ssh/gslave02 -p 2223 nari@140.123.97.173
 
-scp -i ~/.ssh/gslave02 -P 2223 uploads/tomoki.jpg nari@140.123.97.173:~/style_transfer/neural-style
-scp -i ~/.ssh/gslave02 -P 2223 uploads/test_150.jpg nari@140.123.97.173:~/style_transfer/neural-style
+echo $1
+
+#scp -i ~/.ssh/gslave02 -P 2223 uploads/$1 nari@140.123.97.173:~/style_transfer/neural-style
+#scp -i ~/.ssh/gslave02 -P 2223 uploads/test_150.jpg nari@140.123.97.173:~/style_transfer/neural-style
 
 
