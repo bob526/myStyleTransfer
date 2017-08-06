c651fcd855203345c29a90743edf5500976eae4a
nari <invoid@csie.io>
2017-08-07 02:25:48 +0800

    more debug print

--- web/auto_style.sh
+++ web/auto_style.sh
@@ -3,7 +3,9 @@
 
 #ssh -i ~/.ssh/gslave02 -p 2223 nari@140.123.97.173
 
-echo test
+echo test<br>
+echo $0<br>
+echo $1<br>
 scp -i ~/.ssh/gslave02 -P 2223 uploads/$1 nari@140.123.97.173:~/style_transfer/neural-style
 #scp -i ~/.ssh/gslave02 -P 2223 uploads/test_150.jpg nari@140.123.97.173:~/style_transfer/neural-style
 
